78f204b489c536cfcd8076060a95913c1b015a5b
Jonathan Gazeley <me@jonathangazeley.com>
2023-12-20 22:30:19 +0000

    Add docstrings

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+"""
+A simple app to display the current liturgical colour of the Church of England
+"""
 from flask import Flask, render_template
 from liturgical_colour.liturgical import liturgical_colour
 
@@ -5,6 +8,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
+    """
+    Return the liturgical colour
+    """
     dayinfo = liturgical_colour(None)
     longdate = dayinfo['date'].strftime("%A, %-d %B %Y")
     return render_template('template.html', dayinfo=dayinfo, longdate=longdate)
