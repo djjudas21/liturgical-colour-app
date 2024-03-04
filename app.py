ff5c43f0f57160f03d81e2a88fdde079e4612933
Jonathan Gazeley <me@jonathangazeley.com>
2024-03-04 16:55:11 +0000

    Add a date-picker to the site, defaults to today

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 """
 A simple app to display the current liturgical colour of the Church of England
 """
-from flask import Flask, render_template
+from datetime import date
+from flask import Flask, request, render_template
 from liturgical_colour.liturgical import liturgical_colour
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', methods =["GET", "POST"])
 def main():
     """
     Return the liturgical colour
     """
-    dayinfo = liturgical_colour(None)
+    # Get date from datepicker, if set
+    f_date = request.form.get("date") or date.today()
+    dayinfo = liturgical_colour(f_date)
     longdate = dayinfo['date'].strftime("%A, %-d %B %Y")
-    return render_template('template.html', dayinfo=dayinfo, longdate=longdate)
+    return render_template('template.html', dayinfo=dayinfo, longdate=longdate, shortdate=f_date)
