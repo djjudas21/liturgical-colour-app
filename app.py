fda9410f348c14a62a151c13cb8ebef18e63477f
Jonathan Gazeley <me@jonathangazeley.com>
2024-12-29 19:02:01 +0000

    Add manifest and service worker to enable PWA

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 A simple app to display the current liturgical colour of the Church of England
 """
 from datetime import date
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file
 from liturgical_colour.liturgical import liturgical_colour
 
 app = Flask(__name__)
@@ -17,3 +17,17 @@ def main():
     dayinfo = liturgical_colour(f_date)
     longdate = dayinfo['date'].strftime("%A, %-d %B %Y")
     return render_template('template.html', dayinfo=dayinfo, longdate=longdate, shortdate=f_date)
+
+@app.route('/manifest.json')
+def serve_manifest():
+    """
+    Return the PWA manifest.json
+    """
+    return send_file('manifest.json', mimetype='application/manifest+json')
+
+@app.route('/sw.js')
+def serve_sw():
+    """
+    Return the service worker
+    """
+    return send_file('sw.js', mimetype='application/javascript')
